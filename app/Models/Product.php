1af4685b903ebd2d4c2a1f3c849cb9278092565f
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-02-05 07:27:37 +0200

    create new app and add base tables ecommerce

---
+++ app/Models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'images',
+        'price',
+        'quantity',
+        'views',
+
+        'category_id',
+        'user_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_items', 'product_id', 'order_id')
+            ->withPivot('quantity', 'price')
+            ->withTimestamps();
+    }
+}
