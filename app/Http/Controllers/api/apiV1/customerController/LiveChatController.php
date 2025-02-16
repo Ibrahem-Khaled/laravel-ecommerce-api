72ca9f420ee4f601ca5461d18d33085104c6d44b
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-02-16 06:45:12 +0200

    add home page add admin

--- app/Http/Controllers/api/apiV1/customerController/LiveChatController.php
+++ app/Http/Controllers/api/apiV1/customerController/LiveChatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api\apiV1\customerController;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LiveChatController extends Controller
@@ -17,12 +18,14 @@ class LiveChatController extends Controller
     public function store(Request $request)
     {
         $user = auth()->guard('api')->user();
+        $admin = User::where('role', 'admin')->first();
         $chat = $user->chats()->create([
             'message' => $request->message
         ]);
         if ($user->chats()->count() == 1) {
             $user->chats()->create([
-                'message' => 'مرحبا بك في كيف يمكننا مساعدتك'
+                'message' => 'مرحبا بك في كيف يمكننا مساعدتك',
+                'admin_id' => $admin->id,
             ]);
         }
         return response()->json($chat);
