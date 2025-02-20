c451e9b29b1dcfade0b58a678ab82735248cfdeb
Ibrahem-Khaled <xx.vvv48@gmail.com>
2025-02-20 03:42:42 +0200

    add        // إنشاء نسخة من Faker باستخدام اللغة العربية (مثلاً: العربية السعودية)

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,13 +19,16 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        // إنشاء نسخة من Faker باستخدام اللغة العربية (مثلاً: العربية السعودية)
+        $faker = \Faker\Factory::create('ar_SA');
+
         return [
             'sub_category_id' => SubCategory::inRandomOrder()->first()->id ?? SubCategory::factory(),
             'user_id' => User::inRandomOrder()->first()->id ?? User::factory(),
             'name' => $this->faker->word,
             'description' => $this->faker->sentence,
             'status' => $this->faker->randomElement(['active', 'inactive']),
-            'images' => json_encode([$this->faker->imageUrl(400, 400, 'products')]),
+            // 'images' => json_encode([$this->faker->imageUrl(400, 400, 'products')]),
             'price' => $this->faker->randomFloat(2, 10, 1000),
             'quantity' => $this->faker->numberBetween(1, 100),
             'views' => $this->faker->numberBetween(0, 1000),
